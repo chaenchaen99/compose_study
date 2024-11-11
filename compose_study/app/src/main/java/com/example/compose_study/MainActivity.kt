dbee03b9fb23264ceb1ee2c9979c3056ad5aade5
chaeyeon <chaenjung.99@gmail.com>
2024-11-11 14:33:22 +0900

    textfield & scaffold 실습

--- compose_study/app/src/main/java/com/example/compose_study/MainActivity.kt
+++ compose_study/app/src/main/java/com/example/compose_study/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.compose_study
 
+import android.annotation.SuppressLint
 import android.graphics.drawable.Icon
 import android.inputmethodservice.Keyboard
 import android.os.Bundle
@@ -16,18 +17,27 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SegmentedButtonDefaults.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -187,10 +197,51 @@ fun ComposeEx() {
     }
 }
 
+@Composable
+fun TextfieldEx() {
+    var name by remember { mutableStateOf("Tom") }
+    //mutableStateOf
+    //name의 상태를 관리하는 객체를 생성
+    //by 키워드를 통해 name의 상태를 읽고 쓸 수 있게 해준다.
+
+    Column {
+        TextField(value = name, onValueChange = { name = it })
+        Text(text = "Hello $name")
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScaffoldEx() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Image(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "뒤로가기",
+                        )
+                    }
+                },
+                title = {
+                    Text(text = "Scaffold App")
+
+                }
+            )
+        }
+    ) {
+        Surface(modifier = Modifier.padding(8.dp)) {
+
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Compose_studyTheme {
-        CardEx(cardData = cardData)
+        ScaffoldEx()
     }
 }
