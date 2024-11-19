3a43d59471537988a8ee829cc435bf95f0ce1c84
chaeyeon <chaenjung.99@gmail.com>
2024-11-19 11:39:22 +0900

    canvas & dialog 실습

--- compose_study/app/src/main/java/com/example/compose_study/MainActivity.kt
+++ compose_study/app/src/main/java/com/example/compose_study/MainActivity.kt
@@ -6,6 +6,7 @@ import android.inputmethodservice.Keyboard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.content.MediaType.Companion.Image
@@ -15,12 +16,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -40,9 +44,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -238,10 +244,64 @@ fun ScaffoldEx() {
     }
 }
 
+@Composable
+fun CanvasEx() {
+    Canvas(modifier = Modifier.size(20.dp)) {
+        //1.drawLine
+        drawLine(Color.Red, Offset(30f, 10f), Offset(50f, 40f))
+
+        //2.drwaCircle
+        drawCircle(Color.Yellow, 30f, Offset(15f, 15f))
+
+
+    }
+}
+
+@Composable
+fun DialogEx() {
+    var openDialog by remember { mutableStateOf(false) }
+    var counter by remember { mutableStateOf(0) }
+
+    Column {
+        Button(onClick = { openDialog = true }) {
+            Text("다이어로그 열기")
+        }
+        Text("카운터: $counter")
+    }
+
+    if (openDialog) {
+        AlertDialog(
+            onDismissRequest = {
+                openDialog = false
+            },
+            confirmButton = {
+                Button(onClick = {
+                    counter++
+                    openDialog = false
+                }) {
+                    Text("더하기")
+                }
+            },
+            dismissButton = {
+                Button(onClick = {
+                    openDialog = false
+                }) {
+                    Text("취소")
+                }
+            },
+            title = {
+                    Text("더하기")
+            }, text = {
+                Text("더하기 버튼을 누르면 카운터를 증가합니다.\n버튼을 눌러주세요.")
+            },
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Compose_studyTheme {
-        ScaffoldEx()
+        DialogEx()
     }
 }
